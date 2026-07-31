20b502bc7946fe0350caf5a973b4d4196b656c7f
nmsaikat <nmsaikat.cf@gmail.com>
2026-07-31 21:13:32 +0600

    Number Problems — Factorial & FizzBuzz

---
+++ problem-04.js
@@ -0,0 +1,35 @@
+// Problem 4: FizzBuzz (1 to N)
+// loop through numbers form 1 to n. if a number is divisible by 3, print "Fizz"; if divisible by 5, print "Buzz"; if divisible by both, print "FizzBuzz"; otherwise, print the number itself.
+
+// Without Function
+// let number = 15;
+// for (let i = 1; i <= number; i++) {
+//   console.log("=>", i);
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// With Function
+function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++) {
+    console.log("=>", i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(15);
