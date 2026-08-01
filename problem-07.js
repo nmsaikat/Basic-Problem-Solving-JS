f97c0962867f95c86fd69121d298066634a8ab4c
nmsaikat <nmsaikat.cf@gmail.com>
2026-08-01 16:18:14 +0600

    String Problems — Palindrome Check & Count Words

---
+++ problem-07.js
@@ -0,0 +1,7 @@
+// Problem 7: Check if a string is a palindrome
+
+let str = "madam";
+
+let str2 = "madam";
+
+console.log(str === str2);
---
+++ problem-08.js
@@ -0,0 +1,15 @@
+// Problem 8: Count words in s Sentence
+
+// let arr = ["code", "Bangladesh", "Dhaka"];
+// console.log(arr.length);
+
+// Whiteout function
+// let str = "Programming Hero";
+// console.log(str.split(" "));
+
+// With Function
+// function wordCount(str) {
+//   console.log(str.split(""));
+//   return arr.length;
+// }
+// console.log(wordCount("NM Saikat"));
